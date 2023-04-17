40be077884ca536e570e0c8c1e60d14a07b8bac9
Nikhil Dhumal <pekacoclover@gmail.com>
2023-04-17 18:57:43 +0530

    added multithreading and fixed exceptions

--- client.py
+++ client.py
@@ -1,15 +1,19 @@
 import socket
 import re
 
+PATH_ERROR = "#@#".encode()
+EXIT_MESSAGE = "#$#".encode()
+SUCCESS_MESSAGE = "#!#"
+
 client_socket = socket.socket()
 
 # defining port and host
-host = 'localhost'
+host = "localhost"
 port = 12345
 
 # Connect socket to the host and port
 client_socket.connect((host, port))
-print('Connection established...')
+print("Connection established...")
 
 folder_path = input("Enter folder path in which files will be recieved: ")
 if not folder_path[len(folder_path) - 1] == "\\":
@@ -18,30 +22,46 @@ if not folder_path[len(folder_path) - 1] == "\\":
 while True:
     # send a request to the server
     while True:
-        file_path = input("Enter path of file to be received: ")
-        if mathches :=re.search(r".*\\(.*)$", file_path, re.IGNORECASE):
-            folder_path = folder_path + mathches.group(1)
+        file_path = input("Enter path of the file to be received: ")
+        if mathches := re.search(r".*\\(.*)$", file_path, re.IGNORECASE):
+            receive_here = folder_path + mathches.group(1)
             client_socket.send(file_path.encode())
             break
         else:
             print("Invalid path!")
 
-    line = client_socket.recv(1024)
-    message = line.decode()
-    if message == "error_unique":
+    data = client_socket.recv(1024)
+    message = data.decode()
+    if message == "#~#":
         print("[Server]: The specified file path does not exist!")
-        break
+        client_socket.send(PATH_ERROR)
+        continue
 
     try:
         # Write File in binary
-        with open(folder_path, "wb") as file:
-            while(line):
-                file.write(line)
-                line = client_socket.recv(1024)
-        print('File has been received successfully.')
+        with open(receive_here, "wb") as file:
+            message = ""
+            while message != SUCCESS_MESSAGE:
+                file.write(data)
+                data = client_socket.recv(1024)
+                message = data.decode()
+        print("File has been received successfully.")
     except:
-        print("Invalid path for receiving file!")
+        print("Invalid folder path for receiving file!")
+        client_socket.send(PATH_ERROR)
 
-    client_socket.close()
-    print('Connection Closed.')
-    break
+        folder_path = input("Enter folder path in which files will be recieved: ")
+        if not folder_path[len(folder_path) - 1] == "\\":
+            folder_path = folder_path + "\\"
+
+        continue
+
+    choice = input(
+        "Enter 'exit' to end connection or 'continue' to continue receiving files: "
+    )
+    if choice == "exit":
+        break
+
+client_socket.send(EXIT_MESSAGE)
+client_socket.close()
+print("Connection closed.")
--- server.py
+++ server.py
@@ -1,9 +1,53 @@
 import socket
+from _thread import *
+from time import sleep
+
+PATH_ERROR = "#~#".encode()
+EXIT_MESSAGE = "#$#"
+SUCCESS_MESSAGE = "#!#".encode()
+
+
+def handle_new_client(connection, address):
+    print("Connected to: " + address[0] + ":" + str(address[1]))
+
+    try:
+        while True:
+            # Get data from the client
+            req = connection.recv(1024)
+            request = req.decode()
+
+            if request == "#@#":
+                continue
+            elif request == EXIT_MESSAGE:
+                break
+
+            print("[Client]: Requested file path:", request)
+
+            try:
+                # Read File in binary
+                with open(request, "rb") as file:
+                    data = file.read(1024)
+                    # send data to the client
+                    while data:
+                        connection.send(data)
+                        data = file.read(1024)
+                sleep(0.1)
+                connection.send(SUCCESS_MESSAGE)
+                print("File has been transferred successfully.")
+            except:
+                connection.send(PATH_ERROR)
+
+        print(address[0] + ":" + str(address[1]) + " is disconnected...")
+        connection.close()
+    except:
+        print(address[0] + ":" + str(address[1]) + " is disconnected...")
+        connection.close()
+
 
 server_socket = socket.socket()
 
 # defining port and host
-host = 'localhost'
+host = "localhost"
 port = 12345
 
 # binding to the host and port
@@ -11,29 +55,9 @@ server_socket.bind((host, port))
 
 # accept up to 10 connections
 server_socket.listen(10)
-print('Server is listening...')
+print("Server is listening...")
 
 while True:
     # establish connection with the clients.
     connection, address = server_socket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-
-    # Get data from the client
-    req = connection.recv(1024)
-    filepath = req.decode()
-    print("[Client]: Requested file path:", filepath)
-
-    try:
-        # Read File in binary
-        with open(filepath, 'rb') as file:
-            line = file.read(1024)
-            # Keep sending data to the client
-            while(line):
-                connection.send(line)
-                line = file.read(1024)
-        print('[Server] File has been transferred successfully.')
-    except:
-        message = "error_unique".encode()
-        connection.send(message)
-
-    connection.close()
+    start_new_thread(handle_new_client, (connection, address))
